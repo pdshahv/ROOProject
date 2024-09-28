c3a146a5f118d73f03753b1b6064909808be4859
Pankaj <shahpresent@gmail.com>
2024-09-28 16:28:36 +0530

    Generic Entity Classes

---
+++ Shared/DDD/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DDD
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+        public DateTime? CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? LastModified { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? LastModifiedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    }
+}
